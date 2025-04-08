6667bdaa4b5e60ccf87caeebac74f3508fe9924b
Joao <vtrdll3@gmail.com>
2025-04-08 19:20:40 -0300

    Aprendendo como serializers funcionam

---
+++ genres/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from genres.models import Genre
+
+# Register your models here.
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
---
+++ genres/views.py
@@ -0,0 +1,38 @@
+import json
+from django.http import JsonResponse
+from genres.models import Genre
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import  get_object_or_404
+# Create your views here.
+
+@csrf_exempt
+def genre_create_list_view(request):
+
+    if request.method == 'GET':
+        genres = Genre.objects.all()
+        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data['name'])
+        new_genre.save()
+        return JsonResponse({'id': new_genre.id,'name': new_genre.name}, status=201, )
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+
+    if request.method == 'GET':
+        data = {'id': genre.id, 'name': genre.name}
+        return JsonResponse(data)
+
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        genre.name = data['name']
+        genre.save()
+        return JsonResponse({'id': genre.id,'name': genre.name})
+
+    elif request.method == 'DELETE':
+        genre.delete()
+        return JsonResponse({'message': 'Genero excluido'}, status=204)
+
