65803b890d8e7af41b673902c70f90ff3a17875f
Joao <vtr.dll3@gmail.com>
2025-04-24 21:31:09 -0300

    Adicionado validate

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
-
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -12,7 +11,7 @@ urlpatterns = [
     path('api/v1/', include('actors.urls')),
     path('api/v1/', include('movies.urls')),
     path('api/v1/', include('reviews.urls')),
-
+]
 
 
 
--- movies/serializers.py
+++ movies/serializers.py
@@ -8,3 +8,10 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_relase_date(self, value):
+        if value < 1900:
+            raise serializers.ValidationError('A data de lançamento precisa ser menor que 1900.')
+        return value 
+    
+    
